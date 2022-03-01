10bfc3de4b21f1331097908845744562aac22592
Norman JN Richardson <normanrichardson@gmail.com>
2022-03-01 09:47:50 -0800

    Create ERC20 Sofa Token

--- contracts/Sofa.sol
+++ contracts/Sofa.sol
@@ -1,11 +1,11 @@
-// contracts/Soap.sol
+// contracts/DumpsterFire.sol
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Soap is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Soap", "SOA") {
+contract Sofa is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Sofa", "SOF") {
         _mint(msg.sender, initialSupply);
-    } 
+    }
 }
