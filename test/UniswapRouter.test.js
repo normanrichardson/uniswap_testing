a527d484e4c93cba7f2e292e39a81da89b22c397
Norman JN Richardson <normanrichardson@gmail.com>
2022-03-18 19:09:33 -0700

    Update swapper testing to use waffle event matchers

--- test/UniswapRouter.test.js
+++ test/UniswapRouter.test.js
@@ -2,58 +2,58 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
 describe('Swapping weth for dia', () => {
+    let
+        signr,
+        swapperDia,
+        weth,
+        dia,
+        router,
+        poolAddress;
+
     before( async () => {
         const routerAddress = '0xE592427A0AEce92De3Edee1F18E0157C05861564';
-        const wethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-        const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+        const wethAddress   = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
+        const daiAddress    = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+        const factoryAddress = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
         
-        const router = await ethers.getContractAt('ISwapRouter', routerAddress);
-        
+        router = await ethers.getContractAt('ISwapRouter', routerAddress);
         const Swapper = await ethers.getContractFactory('Swapper');
+        const factory = await ethers.getContractAt('IUniswapV3Factory', factoryAddress);
         
-        this.swapperDia = await Swapper.deploy(router.address, wethAddress, daiAddress, 3000);
-        this.weth = await ethers.getContractAt('DepositableERC20', wethAddress);
-        this.dia = await ethers.getContractAt('IERC20', daiAddress);
+        swapperDia = await Swapper.deploy(router.address, wethAddress, daiAddress, 3000);
+        poolAddress = await factory.getPool(wethAddress, daiAddress, 3000);
+        weth = await ethers.getContractAt('DepositableERC20', wethAddress);
+        dia = await ethers.getContractAt('IERC20', daiAddress);
 
         // get signers for testing
-        const [signr1, signr2, signr3] = await ethers.getSigners();
-
-        this.signr1 = signr1;
-        this.signr2 = signr2;
-        this.signr3 = signr3;
+        [, , signr] = await ethers.getSigners();
     });
 
     it('Single exact input swap WETH for DIA', async () => {
-        const signr = this.signr3;
         const ethTransfer = ethers.BigNumber.from('1');
         const weiTransfer = ethTransfer.mul(ethers.constants.WeiPerEther);
+        const diaBefore = await dia.balanceOf(signr.address);
 
-        await this.weth.connect(signr).deposit({value: weiTransfer});
-
-        const diaBefore = await this.dia.balanceOf(signr.address);
-
-        await this.weth.connect(signr).approve(this.swapperDia.address, weiTransfer);
-        await this.swapperDia.connect(signr).swapExactInputSingle(weiTransfer);
-
-        const diaAfter = await this.dia.balanceOf(signr.address);
-
+        await weth.connect(signr).deposit({value: weiTransfer});
+        await weth.connect(signr).approve(swapperDia.address, weiTransfer);
+        await expect(swapperDia.connect(signr).swapExactInputSingle(weiTransfer))
+            .to.emit(weth, 'Transfer')
+            .withArgs(signr.address, swapperDia.address, weiTransfer);
+        
+        const diaAfter = await dia.balanceOf(signr.address);
+        
         expect(diaBefore).to.lt(diaAfter);
     });
 
     it('Single exact output swap WETH for DIA', async () => {
-        const signr = this.signr3;
-        const diaOut = 1;
-        const ethMax = 0.00000001;
-        const weiMax = ethMax*ethers.constants.WeiPerEther;
-        const tx = await this.weth.connect(signr).deposit({value: weiMax});
+        const diaOut = ethers.BigNumber.from('1000');
+        const ethMax = ethers.BigNumber.from('1');
+        const weiMax = ethMax.mul(ethers.constants.WeiPerEther);
 
-        const diaBefore = await this.dia.balanceOf(signr.address);
-
-        await this.weth.connect(signr).approve(this.swapperDia.address, weiMax);
-        await this.swapperDia.connect(signr).swapExactOutputSingle(diaOut,weiMax);
-
-        const diaAfter = await this.dia.balanceOf(signr.address);
-        
-        expect(diaAfter.sub(diaBefore)).to.equal(diaOut);
+        await weth.connect(signr).deposit({value: weiMax});
+        await weth.connect(signr).approve(swapperDia.address, weiMax);
+        await expect(swapperDia.connect(signr).swapExactOutputSingle(diaOut,weiMax))
+            .to.emit(dia, 'Transfer')
+            .withArgs(poolAddress, signr.address, diaOut);
     });
 });
