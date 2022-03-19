2aec0e93b3a41266cfa3c1d206ee3ca905cca81f
Norman JN Richardson <normanrichardson@gmail.com>
2022-03-18 18:24:21 -0700

    Update Weth testing to use waffle event matchers

--- contracts/DepositableERC20.sol
+++ contracts/DepositableERC20.sol
@@ -7,6 +7,8 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // Add deposit function for WETH
 interface DepositableERC20 is IERC20 {
+  event  Deposit(address indexed dst, uint wad);
+  event  Withdrawal(address indexed src, uint wad);
   function deposit() external payable;
   function withdraw(uint wad) external;
   function decimals() external view returns (uint8);
--- test/weth.test.js
+++ test/weth.test.js
@@ -4,78 +4,67 @@ const { ethers } = require('hardhat');
 require('dotenv').config();
 
 describe('Test WETH', () => {
+    let 
+        weth,
+        signr1,
+        signr2,
+        signr3;
     
     before(async () => {
         // weth address on rinkeby.
-        const wethAddress = '0xc778417E063141139Fce010982780140Aa0cD5Ab';
+        const wethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
 
         // populate the DepositableERC20 interface
-        this.weth = await ethers.getContractAt('DepositableERC20', wethAddress);
+        weth = await ethers.getContractAt('DepositableERC20', wethAddress);
 
         // get signers for testing
-        const [owner, other1, other2] = await ethers.getSigners();
-
-        this.owner = owner;
-        this.other1 = other1;
-        this.other2 = other2;
+        [signr1, signr2, signr3] = await ethers.getSigners();
     });
 
     it('Deposit WETH', async () => {
-        const eth_before = await this.owner.getBalance();
-        const weth_before = await this.weth.balanceOf(this.owner.address);
-        const wei_transfer = ethers.utils.parseEther('0.001');
-        const tx = await this.weth.connect(this.owner).deposit({value: wei_transfer});
-
-        const eth_after = await this.owner.getBalance();
-        const weth_after = await this.weth.balanceOf(this.owner.address);
+        const ethTransfer = ethers.BigNumber.from('2');
+        const weiTransfer = ethers.constants.WeiPerEther.mul(ethTransfer);
         
-        expect(weth_after - weth_before).to.equal(wei_transfer);
-        expect(eth_before - eth_after).to.gt(wei_transfer); 
+        await expect( await weth.connect(signr1).deposit({value: weiTransfer}))
+            .to.emit(weth, 'Deposit')
+            .withArgs(signr1.address, weiTransfer);
     });
         
     it('Transfer WETH by owner', async () => {
         // Participants
-        const p1 = this.owner;
-        const p2 = this.other1;
-        const p1WethBefore = await this.weth.balanceOf(p1.address);
-        const p2WethBefore = await this.weth.balanceOf(p2.address);
-        // Dealing with bignumber casting issues
-        const transAmm = p1WethBefore > ethers.utils.parseEther('0.001') ? ethers.utils.parseEther('0.001') : p1WethBefore/4;
+        const p1 = signr1;
+        const p2 = signr2;
+        const transAmm = ethers.BigNumber.from('1');
+        const weiTransfer = ethers.constants.WeiPerEther.mul(transAmm).div(2);
 
-        await this.weth.transfer(p2.address, transAmm);
-        const p1WthAfter = await this.weth.balanceOf(p1.address);
-        const p2WthAfter = await this.weth.balanceOf(p2.address);
-
-        expect(p1WethBefore-p1WthAfter-transAmm).to.equal(0);
-        expect(p2WthAfter-p2WethBefore-transAmm).to.equal(0);
+        await expect(weth.transfer(p2.address, weiTransfer))
+            .to.emit(weth, 'Transfer')
+            .withArgs(p1.address, p2.address, weiTransfer);
     });
 
     it('Transfer WETH by 3rd party', async () => {
         // Participants
-        const p1 = this.owner;
-        const p2 = this.other1;
-        const p3 = this.other2;
-        const p1WethBefore = await this.weth.balanceOf(p1.address);
-        const p3WethBefore = await this.weth.balanceOf(p3.address);
-        // Dealing with bignumber casting issues
-        const transAmm = p1WethBefore > ethers.utils.parseEther('0.001') ? ethers.utils.parseEther('0.001') : p1WethBefore/4;
+        const p1 = signr1;
+        const p2 = signr2;
+        const p3 = signr3;
+        const transAmm = ethers.BigNumber.from('1');
+        const weiTransfer = ethers.constants.WeiPerEther.mul(transAmm).div(2);
 
-        await this.weth.connect(p1).approve(p2.address, transAmm);
-        await this.weth.connect(p2).transferFrom(p1.address, p3.address, transAmm);
+        await expect(weth.connect(p1).approve(p2.address, weiTransfer))
+            .to.emit(weth, 'Approval')
+            .withArgs(p1.address, p2.address, weiTransfer);
 
-        const p1WthAfter = await this.weth.balanceOf(p1.address);
-        const p3WthAfter = await this.weth.balanceOf(p3.address);
-
-        expect(p1WethBefore-p1WthAfter-transAmm).to.equal(0);
-        expect(p3WthAfter-p3WethBefore-transAmm).to.equal(0);
+        await expect(weth.connect(p2).transferFrom(p1.address, p3.address, weiTransfer))
+            .to.emit(weth, 'Transfer')
+            .withArgs(p1.address, p3.address, weiTransfer);
     });
 
     it('Withdraw ETH', async () => {
-        const balance = await this.weth.balanceOf(this.owner.address);
+        const balance = await weth.balanceOf(signr1.address);
         
-        await this.weth.withdraw(balance);
-        const bal_after = await this.weth.balanceOf(this.owner.address);
-
-        expect(bal_after).to.equal(0);
+        await expect(weth.withdraw(balance))
+            .to.emit(weth, 'Withdrawal')
+            .withArgs(signr1.address, balance);
+        
     });
 });
